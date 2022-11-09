9b9310d9e37e43d887866507be99641d68a179a8
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-09 18:31:28 +0300

    Format

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         int sum = 100;
         int money = 1100;
         int bonus = 100;
         int bonusMoney = 0;
         System.out.println("Счет абонента: " + sum + " руб.");
         System.out.println("Сумма пополнения: " + money + " руб.");
-        if (money > 1000)
-        {
+        if (money > 1000) {
             bonusMoney = money / bonus;
-        }
-        else
-        {
+        } else {
             bonusMoney = 0;
         }
         sum = sum + money + bonusMoney;
